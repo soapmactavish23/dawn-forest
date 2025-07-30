39bd361cfbe48697f7c2d770d5b59f246133909d
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-07-30 19:51:24 -0300

    39. Script Inicial do Personagem | Movimentação Horizontal.

---
+++ scripts/player/player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+class_name Player
+
+onready var player_sprite: Sprite = $Texture
+
+var velocity: Vector2
+
+export(int) var speed
+
+func _physics_process(delta):
+	horizontal_movement_env()
+	velocity = move_and_slide(velocity)
+	
+func horizontal_movement_env() -> void:
+	var uiRight = Input.get_action_strength("ui_right")
+	var uiLeft = Input.get_action_strength("ui_left")
+	var input_direction: float = uiRight - uiLeft
+	
+	velocity.x = input_direction * speed
+	print(velocity.x)
+	
