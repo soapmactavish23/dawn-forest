ea8a9089d7b414cd470589c1abcbc429f1c92318
henrick.nogueira <ricknogueira1231@gmail.com>
2025-08-05 19:31:02 -0300

    42. Movimentação na vertical do personagem

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -5,10 +5,20 @@ onready var player_sprite: Sprite = $Texture
 
 var velocity: Vector2
 
+var jump_count: int = 0
+
+var lading: bool = false
+
 export(int) var speed
+export(int) var jump_speed
+
+export(int) var player_gravity
 
 func _physics_process(delta):
 	horizontal_movement_env()
+	vertical_movement_env()
+	
+	gravity(delta)
 	velocity = move_and_slide(velocity)
 	player_sprite.animate(velocity)
 	
@@ -19,4 +29,16 @@ func horizontal_movement_env() -> void:
 	
 	velocity.x = input_direction * speed
 	print(velocity.x)
+
+func vertical_movement_env() -> void:
+	if is_on_floor():
+		jump_count = 0
 	
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+		jump_count += 1
+		velocity.y = jump_speed
+
+func gravity(delta: float) -> void:
+	velocity.y += delta * player_gravity
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
