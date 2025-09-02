905677ba3f5d08dd959916fac1473343b6043c5e
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-01 22:23:24 -0300

    65. Script de Detecção do Personagem.

---
+++ scripts/enemy/detection_area.gd
@@ -0,0 +1,12 @@
+extends Area2D
+class_name DetectionArea
+
+export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
+
+
+func _on_body_entered(body: Player) -> void:
+	enemy.player_ref = body
+
+
+func _on_body_exited(body):
+	enemy.player_ref = null
