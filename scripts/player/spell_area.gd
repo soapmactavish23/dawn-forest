f96eab825f38e19b1a6be1d28468ebf22c0cf6bc
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-22 00:45:40 -0300

    99. Criando o Script do Feitiço.

---
+++ scripts/player/spell_area.gd
@@ -0,0 +1,13 @@
+extends Area2D
+class_name FireSpell
+
+var spell_damage: int
+
+func _ready():
+	for children in get_children():
+		if children is Particles2D:
+			children.emitting = true
+
+
+func _on_animation_finished(anim_name: String) -> void:
+	queue_free()
