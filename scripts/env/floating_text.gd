0b005280b6f9342271ed23d58634b2cefca44b9a
henrick.nogueira <ricknogueira1231@gmail.com>
2025-09-28 00:54:00 -0300

    104. Script Principal da Popup de Texto.

--- scripts/env/floating_text.gd
+++ scripts/env/floating_text.gd
@@ -3,7 +3,7 @@ class_name FloatText
 
 onready var tween: Tween = get_node("Tween")
 
-var values: int
+var value: int
 var mass: int = 20
 
 var velocity: Vector2
@@ -16,3 +16,68 @@ export(Color) var exp_color
 export(Color) var heal_color
 export(Color) var mana_color
 export(Color) var damage_color
+
+func _ready():
+	randomize()
+	velocity = Vector2(
+		rand_range(-10, 10),
+		-30
+	)
+	
+	floating_text()
+	
+func floating_text() -> void:
+	text = type_sign + str(value)
+	match type:
+		"Exp":
+			modulate = exp_color
+		"Heal":
+			modulate = heal_color
+		"Mana":
+			modulate = mana_color
+		"Damage":
+			modulate = damage_color
+		
+	interpolate()
+	
+func interpolate() -> void:
+	var _interpolate_module: bool = tween.interpolate_property(
+		self, 
+		"modulate:a", 
+		1.0, 
+		0.0, 
+		0.3, 
+		Tween.TRANS_LINEAR, 
+		Tween.EASE_OUT, 
+		0.7
+	)
+	
+	var _interpolate_scale_up: bool = tween.interpolate_property(
+		self, 
+		"rect_scale", 
+		Vector2(0.0, 0.0),
+		Vector2(1.0, 1.0),
+		0.3, 
+		Tween.TRANS_LINEAR, 
+		Tween.EASE_OUT
+	)
+	
+	var _interpolate_scale_down: bool = tween.interpolate_property(
+		self, 
+		"rect_scale", 
+		Vector2(1.0, 1.0),
+		Vector2(0.4, 0.4),
+		1.0, 
+		Tween.TRANS_LINEAR, 
+		Tween.EASE_OUT,
+		0.6
+	)
+	
+	var _start: bool = tween.start()
+	
+	yield(tween, "tween_all_completed")
+	queue_free()
+
+func _process(delta):
+	velocity += gravity * mass * delta
+	rect_position += velocity * delta
