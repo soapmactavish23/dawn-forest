fed4e4a36ea560dfee059a524a40bccd004e7bb4
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-21 21:03:15 -0300

    115. Configurando os Itens a serem Dropados pelo Inimigo.

--- scripts/enemy/pink_start/pink_start.gd
+++ scripts/enemy/pink_start/pink_start.gd
@@ -1,3 +1,57 @@
 extends EnemyTemplate
 class_name PinkStar
 
+func _ready():
+	randomize()
+	drop_list = {
+		"Heal Potion": [
+			"res://assets/item/consumable/health_potion.png",
+			25,
+			"Health",
+			5,
+			2
+		],
+		"Mana Potion": [
+			"res://assets/item/consumable/mana_potion.png",
+			12,
+			"Mana",
+			5,
+			5
+		],
+		"Pink Start Mouth": [
+			"res://assets/item/resource/pink_star/pink_star_mouth.png",
+			47,
+			"Resource",
+			{},
+			5
+		],
+		"Pink Star Bow": [
+			"res://assets/item/equipment/pink_star_bow.png",
+			1,
+			{
+				"Attack": 5
+			},
+			60
+		],
+		"Pink Star Belt": [
+			"res://assets/item/equipment/pink_star_belt.png",
+			3,
+			"Equipament",
+			{
+				"Health": 5,
+				"Mana": 5
+			},
+			40
+		],
+		"Pink Star Shield": [
+			"res://assets/item/equipment/pink_star_shield.png",
+			1,
+			"Weapon",
+			{
+				"Health": 3,
+				"Defense": 2
+			},
+			75
+		]
+		
+	}
