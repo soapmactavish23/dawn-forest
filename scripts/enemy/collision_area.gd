6099d3f63dbd428f8be4af0dd1285d03aefbf495
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-24 16:31:05 -0300

    120. Criando o Script e Testando a Barra de Vida.

--- scripts/enemy/collision_area.gd
+++ scripts/enemy/collision_area.gd
@@ -6,6 +6,10 @@ onready var timer: Timer = $Timer
 export(int) var health
 export(float) var invulnerability_timer
 export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
+export(NodePath) onready var enemy_bar = get_node(enemy_bar) as Control
+
+func _ready():
+	enemy_bar.init_bar(health)
 
 func on_collision_area_entered(area):
 	if area.get_parent() is Player:
@@ -19,6 +23,7 @@ func on_collision_area_entered(area):
 	
 func update_health(damage: int) -> void:
 	health -= damage
+	enemy_bar.update_bar(health)
 	enemy.spawn_floating_text("-", "Damage", damage)
 	if health <= 0:
 		enemy.can_die = true
