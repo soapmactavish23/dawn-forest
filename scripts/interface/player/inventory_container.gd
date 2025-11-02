d62a72d339e3e8983314b7b336de6823e18602be
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-01 22:01:11 -0300

    124. Script do Inventário - Configurando o Slot [Parte 1].

--- scripts/interface/player/inventory_container.gd
+++ scripts/interface/player/inventory_container.gd
@@ -73,6 +73,23 @@ var slot_list: Array = [
 func _ready():
 	for children in slot_container.get_children():
 		children.connect("empty_slot", self, "empty_slot")
+		
+
+func update_slot(item_name: String, item_image: StreamTexture, item_info: Array):
+	var existing_item_index: int = slot_list.find(item_name)
+	if existing_item_index != -1:
+		var item_slot: TextureRect = slot_container.get_child(existing_item_index)
+		if item_slot.amount < 9 and item_slot.item_type != "Equipament" and item_slot.item_type != "Weapon":
+			var current_amount: int = item_slot.amount + item_info[4]
+			if current_amount > 9:
+				var leftover: int = current_amount - 9
+				item_info[3] = 9 - item_slot.amount
+				item_slot.update_item(item_name, item_image, item_info)
+				item_info[3] = leftover
+				update_slot(item_name, item_image, item_info)
+				return
+			item_slot.update_item(item_name, item_image, item_info)
+			return
 
 func empty_slot(index: int) -> void:
 	slot_list[index] = ""
