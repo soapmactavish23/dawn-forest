07133d64bd71babd1ca67d426c7cf32579356e35
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-08 20:14:49 -0300

    127. Script do Item - Principais Funções

--- scripts/interface/player/item.gd
+++ scripts/interface/player/item.gd
@@ -39,3 +39,65 @@ func on_mouse_entered():
 func on_mouse_exited():
 	can_click = false
 	modulate.a = 1.0
+
+func update_item(
+	item: String, 
+	item_image: StreamTexture, 
+	item_info: Array) -> void:
+	
+	item_image_path = item_info[0]
+	item_type = item_info[1]
+	
+	match item_type:
+		"Equipament":
+			amount = 1
+			item_dictionary = item_info[2]
+		"Weapon":
+			amount = 1
+			item_dictionary = item_info[2]
+		"Resource":
+			amount += item_info[4]
+			type_value = item_info[2]
+		"Health":
+			amount += item_info[4]
+			type_value = item_info[2]
+		"Mana":
+			amount += item_info[4]
+			type_value = item_info[2]
+			
+	sell_price = item_info[3]
+	
+	item_name = item
+	item_amount.text = str(amount)
+	item_texture.texture = item_image
+	
+	if amount != 0 and item_type != "Equipament" and item_type != "Weapon":
+		item_amount.show()
+		item_texture.show()
+		return
+		
+	if item_type == "Equipament" or item_type == "Weapon":
+		item_texture.show()
+	
+	
+func update_slot() -> void:
+	item_amount.hide()
+	item_texture.hide()
+	
+	amount = 0
+	
+	item_name = ""
+	item_type = ""
+	item_image_path = ""
+	
+	type_value = 0
+	sell_price = 0
+	
+	emit_signal("empty_slot", item_index)
+
+
+
+
+
+
+
