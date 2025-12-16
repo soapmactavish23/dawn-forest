ff0f1d111cc2ea491b726b968050d40354b1951f
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-16 00:01:11 -0300

    155. Código Inicial de Serialização

---
+++ scripts/management/data_management.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var save_path: String = 'user://save.dat'
+
+var data_dictionary: Dictionary = {
+	"player_texture": "",
+	"player_position": Vector2(27, 100)
+}
+
+func save_data() -> void:
+	var file: File = File.new()
+	var error = file.open(save_path, File.WRITE)
+	if error == OK:
+		file.store_var(data_dictionary)
+		file.close()
+
+func load_data() -> void:
+	var file: File = File.new()
+	if file.file_exists(save_path):
+		var error = file.open(save_path, File.READ)
+		if error == OK:
+			data_dictionary = file.get_var()
+			file.close()
