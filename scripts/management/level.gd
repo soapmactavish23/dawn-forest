670399e6db772474cad3b02019e76e6d7fc0d4e9
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-18 23:46:43 -0300

    156. Salvando a posição do personagem ao

--- scripts/management/level.gd
+++ scripts/management/level.gd
@@ -7,5 +7,13 @@ func _ready() -> void:
 	var _game_over: bool = player.get_node("Texture").connect("game_over", self, 
 	"on_game_over")
 	
+	data_management.load_data()
+	
+	player.global_position = data_management.data_dictionary.player_position
+	
 func on_game_over() -> void:
 	var _reload: bool = get_tree().reload_current_scene()
+
+func _exit_tree():
+	data_management.data_dictionary.player_position = player.global_position
+	data_management.save_data()
