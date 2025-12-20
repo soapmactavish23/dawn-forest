50ec5cf38e72ec57df38119918ec85198231ef62
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-19 21:29:54 -0300

    157. Reset para a posição inicial ao morrer.

--- scripts/management/level.gd
+++ scripts/management/level.gd
@@ -3,6 +3,8 @@ class_name Level
 
 onready var player: KinematicBody2D = $Player
 
+var is_dead: bool = false
+
 func _ready() -> void:
 	var _game_over: bool = player.get_node("Texture").connect("game_over", self, 
 	"on_game_over")
@@ -12,8 +14,14 @@ func _ready() -> void:
 	player.global_position = data_management.data_dictionary.player_position
 	
 func on_game_over() -> void:
+	data_management.data_dictionary.player_position = data_management.initial_position
+	data_management.save_data()
+	is_dead = true
 	var _reload: bool = get_tree().reload_current_scene()
 
 func _exit_tree():
+	if is_dead == true:
+		return
+		
 	data_management.data_dictionary.player_position = player.global_position
 	data_management.save_data()
